0b39f621d4147add75934624f3ce659a958414ee
chenming18yidong <1070392371@qq.com>
2021-04-29 16:18:05 +0800

    111

--- PC/src/views/Help/help.vue
+++ PC/src/views/Help/help.vue
@@ -1,35 +1,34 @@
 <template>
   <div>
     <div class="helpBody">
-      <div class="helpBody_left">
-        <ul>
-          <li>
-            <span @click="start_suer()" class="user">开始使用</span>
-            <div class="start_use">
-              <ul>
-                <li @click="showdata(1)" :class="this.show === 1 ? $style.text_color : $style.text_color">关于应用</li>
-                <li @click="showdata(2)">主要特点</li>
-                <li @click="showdata(3)">如何开始</li>
-              </ul>
-            </div>
-          </li>
-          <li>aaa</li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <!-- <li @click="showdata(1)">1</li>
-          <li @click="showdata(2)">2</li>
-          <li @click="showdata(3)">3</li> -->
-        </ul>
-      </div>
-      <div class="helpBody_right">
-        <div class="helpBody_right_one" v-show="show === 1"></div>
-        <div class="helpBody_right_two" v-show="show === 2"></div>
-        <div class="helpBody_right_three" v-show="show === 3"></div>
+      <div class="helpBody_box">
+        <div class="helpBody_left">
+          <ul>
+            <li>
+              <span @click="start_suer()" class="user">开始使用</span>
+              <div class="start_use">
+                <ul>
+                  <li @click="showdata(1)" :class="this.show === 1 ? 'text_color' : ' '" >  关于应用  </li>
+                  <li @click="showdata(2)" :class="this.show === 2 ? 'text_color' : ' '" >主要特点</li>
+                  <li @click="showdata(3)" :class="this.show === 3 ? 'text_color' : ' '" >如何开始</li>
+                </ul>
+              </div>
+            </li>
+            <li>制作试卷</li>
+            <li>组织考试</li>
+            <li>创建群组</li>
+            <li>交易试卷</li>
+            <li>离线刷题</li>
+            <li>章节练习</li>
+            <li>定制小程序</li>
+            <li>常见问题</li>
+          </ul>
+        </div>
+        <div class="helpBody_right">
+          <div class="helpBody_right_one" v-show="show === 1"></div>
+          <div class="helpBody_right_two" v-show="show === 2"></div>
+          <div class="helpBody_right_three" v-show="show === 3"></div>
+        </div>
       </div>
     </div>
   </div>
@@ -54,18 +53,24 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-.helpBody {
+
+.helpBody{
+  width: 100%;
+   background: #f7f7f7;
+}
+.helpBody_box {
   display: flex;
   flex-wrap: nowrap;
-  justify-content: space-between;
+  justify-content: flex-start;
   width: 1330px;
-  margin: 0 auto;
+  margin: 0 auto ;
+  padding: 10px 0 0 0;
 }
 
 .helpBody_left {
   width: 170px;
   background: #fbfbfb;
-  .user{
+  .user {
     font-size: 16px;
     display: inline-block;
     width: 100%;
@@ -74,17 +79,20 @@ export default {
     color: #fff;
     padding-left: 20px;
   }
-  .start_use{
+  li {
+    font-size: 16px;
+  }
+  .start_use {
     display: none;
-    ul li{
+    ul li {
       padding-left: 20px;
       color: #ababab;
       font-size: 14px;
     }
   }
 }
-.text_color{
-  color: #ff8200;
+.text_color {
+  color: #ff8200 !important;
 }
 
 .helpBody_left li {
@@ -93,11 +101,12 @@ export default {
   cursor: pointer;
 }
 .helpBody_right {
+  padding-left: 20px;
   width: 500px;
   .helpBody_right_one {
     width: 100%;
     height: 500px;
-    background-color: blue;
+    background: #fff;
   }
   .helpBody_right_two {
     width: 100%;
