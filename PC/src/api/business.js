3ac2883d48034c472eb80056269ecd951c7717d1
chenming18yidong <1070392371@qq.com>
2021-05-10 18:03:15 +0800

    2021.05.10

--- PC/src/api/business.js
+++
@@ -1,9 +0,0 @@
-import { stsqHttp } from '@/utils/ajax'
-
-const stsqReq = stsqHttp.Ajax
-
-/*
-  *https://stsqapi.tibosi.com/user/addBusinessInfo
-  提交信息
-  */
-export const addBusinessInfo = (businessName, linkman, linkNumber) => stsqReq('/user/addBusinessInfo', { businessName, linkman, linkNumber })
